9c69a7796fcc461643231557cf093dc6019e5a25
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-17 11:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+let alphaNumerics = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+let result =''
+
 function makeid(l) {
-  // write your code here
+
+	for(let i=0;i<l;i++){
+		let randomIndex = Math.floor(Math.random()*alphaNumerics.length)
+		result+=alphaNumerics.charAt(randomIndex);
+	}
+
+	return result;
+
 }
 
 // Do not change the code below.
